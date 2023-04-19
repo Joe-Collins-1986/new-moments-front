2fe25b223a7bd82ead403f38e666ae9511b636da
Joseph Collins <joecollins@Josephs-MBP.home>
2023-04-19 15:24:17 +0100

    add logged in navbar icons

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,13 +1,12 @@
 import axios from "axios";
-const jwtToken = localStorage.getItem('jwtToken');
-console.log(jwtToken)
+// const jwtToken = localStorage.getItem('jwtToken');
 
 axios.defaults.baseURL = "https://moments-photos.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+// axios.defaults.xsrfCookieName = 'csrftoken';
+// axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
