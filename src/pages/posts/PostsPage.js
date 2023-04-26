fcafbd39f4407b3dbb4f11066b471b0051ec7056
Joseph Collins <joecollins@Josephs-MBP.home>
2023-04-26 12:13:27 +0100

    final small updates

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -18,12 +18,15 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfile from "../profiles/PopularProfiles";
 
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
   const [query, setQuery] = useState("");
+  const currentUser = useCurrentUser();
 
 
   useEffect(() => {
@@ -44,7 +47,7 @@ function PostsPage({ message, filter = "" }) {
     return () => {
         clearTimeout(timer)
     }
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser]);
 
   return (
     <Row className="h-100">
