2f81cd9fbccc13daab72a6f85d73938389816ec8
Joseph Collins <joecollins@Josephs-MBP.home>
2023-04-26 12:51:35 +0100

    remove uneeded axios defaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,12 +1,8 @@
 import axios from "axios";
-// const jwtToken = localStorage.getItem('jwtToken');
 
 axios.defaults.baseURL = "https://moments-photos.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
-
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
