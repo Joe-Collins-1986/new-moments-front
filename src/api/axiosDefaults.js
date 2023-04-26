14a0b59861ffb855d7f1f5a4c8ba8170c72ea16d
Joseph Collins <joecollins@Josephs-MBP.home>
2023-04-26 12:47:48 +0100

    add axios hear defaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -5,8 +5,8 @@ axios.defaults.baseURL = "https://moments-photos.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-// axios.defaults.xsrfCookieName = 'csrftoken';
-// axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
